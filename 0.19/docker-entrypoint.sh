2b157c5c57c76ef4b3fb2235c228c753d22b7a86
Olli Janatuinen <olli.janatuinen@gmail.com>
2019-12-29 16:23:17 +0000

    Restructured 0.19 Debian build to support ARM builds

--- 0.19/docker-entrypoint.sh
+++ 0.19/docker-entrypoint.sh
@@ -9,8 +9,6 @@ fi
 
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
   mkdir -p "$BITCOIN_DATA"
-  chmod 700 "$BITCOIN_DATA"
-  chown -R bitcoin "$BITCOIN_DATA"
 
   echo "$0: setting data directory to $BITCOIN_DATA"
 
@@ -19,7 +17,7 @@ fi
 
 if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
   echo
-  exec gosu bitcoin "$@"
+  exec "$@"
 fi
 
 echo
